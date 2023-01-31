e9b536c43d1a6644f5e150c9264880e7bc0c0fd7
Swapnil Babladkar <swapnilbabladkar@Swapnils-MacBook-Pro.local>
2023-01-31 14:07:04 +0530

    Simulating layers test -3

--- src/build_deploy.sh
+++ src/build_deploy.sh
@@ -35,6 +35,7 @@ if echo "${changed_service_list[@]}" | grep -q "layers"; then
 
     # Combine the results
     folders="$folders1 $folders2"
+    echo $folders
     changed_folders=$(echo "$folders" | sed 's/\.//g' | sed 's/\///g')
 
     echo ${changed_folders[@]}
